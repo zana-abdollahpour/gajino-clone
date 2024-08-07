a12f85c1b8ec76c75b752475419b589087e93fb1
zana-abdollahpour <zana.ap4@gmail.com>
2024-08-07 08:51:25 +0330

    add Shabnam font as the main font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,36 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const shabnamFont = localFont({
+  src: [
+    {
+      path: "../fonts/shabnam/Shabnam.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/shabnam/Shabnam-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/shabnam/Shabnam-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../fonts/shabnam/Shabnam-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/shabnam/Shabnam-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Gajino clone",
@@ -16,7 +44,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={shabnamFont.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>initial commit</h1>;
+  return <h1>سلام و این برای تست فونت شبنم است</h1>;
 }

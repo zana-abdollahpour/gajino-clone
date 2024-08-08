53468d31bf3705ee144f217b1eab660e0dc10e27
zana-abdollahpour <zana.ap4@gmail.com>
2024-08-08 22:40:43 +0330

    add MoreCategories section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Categories from "@/components/categories/Categories";
 import Discounts from "@/components/discounts/Discounts";
 import Courses from "@/components/courses/Courses";
 import TopTeachers from "@/components/top-teachers/TopTeachers";
+import MoreCategories from "@/components/more-categories/MoreCategories";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Discounts />
       <Courses />
       <TopTeachers />
+      <MoreCategories />
     </>
   );
 }
---
+++ src/components/more-categories/MoreCategories.tsx
@@ -0,0 +1,42 @@
+import SliderWrapper from "@/components/ui/SliderWrapper";
+import Card from "@/components/ui/Card";
+import type { SwiperBreakPoints } from "@/components/ui/SliderWrapper";
+
+import programmingImage from "./img/programming.png";
+import confectioneryImage from "./img/confectionery.png";
+import voiceoverImage from "./img/voiceover.png";
+import sketchingImage from "./img/sketching.png";
+
+const moreCategories = [
+  { name: "برنامه نویسی", image: programmingImage },
+  { name: "شیرینی پزی", image: confectioneryImage },
+  { name: "گویندگی", image: voiceoverImage },
+  { name: "نقاشی سیاه قلم", image: sketchingImage },
+];
+
+const breakPoints: SwiperBreakPoints = {
+  375: { spaceBetween: 6, slidesPerView: 2.2 },
+  768: { spaceBetween: 9, slidesPerView: 3.2 },
+  1024: { spaceBetween: 12, slidesPerView: 4 },
+};
+
+export default function MoreCategories() {
+  return (
+    <div className="mr-auto w-11/12">
+      <SliderWrapper breakPoints={breakPoints}>
+        {moreCategories.map((category, i) => (
+          <div
+            key={category.name}
+            className={`max-w-72 ${i % 2 === 0 ? "-translate-y-12" : "translate-y-20"} pb-20 pt-12`}
+          >
+            <Card src={category.image} alt={category.name}>
+              <span className="inline-block py-4 text-accent-500">
+                {category.name}
+              </span>
+            </Card>
+          </div>
+        ))}
+      </SliderWrapper>
+    </div>
+  );
+}

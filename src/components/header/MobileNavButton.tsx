f286d3ed368bf7f8c394b80ca1707253042c6db7
zana-abdollahpour <zana.ap4@gmail.com>
2024-08-09 14:34:34 +0330

    remove Portal in MobileNavButton to fix hydration error

--- src/components/header/MobileNavButton.tsx
+++ src/components/header/MobileNavButton.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useState } from "react";
-import { createPortal } from "react-dom";
+import { useEffect, useState } from "react";
 import type { MouseEventHandler } from "react";
 
 interface MobileNavButtonProps {
@@ -11,31 +10,37 @@ interface MobileNavButtonProps {
 export default function MobileNavButton({ children }: MobileNavButtonProps) {
   const [navIsOpen, setNavIsOpen] = useState<boolean>(false);
 
+  // prevents user from scrolling when navbar in mobile is open
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    window.document.body.classList.toggle("overflow-hidden");
+  }, [navIsOpen]);
+
   const handleToggle: MouseEventHandler<HTMLDivElement> = (e) => {
     if (e.target !== e.currentTarget) return;
     setNavIsOpen((prev) => !prev);
   };
 
-  if (typeof window === "undefined") return;
-
-  return document ? (
+  return (
     <div
       className="flex h-6 w-5 cursor-pointer flex-col justify-center gap-1 lg:hidden"
       onClick={handleToggle}
     >
-      <div className="-z-10 h-[3px] w-full rounded-full bg-primary-600" />
-      <div className="-z-10 h-[3px] w-full rounded-full bg-primary-600" />
-      <div className="-z-10 h-[3px] w-full rounded-full bg-primary-600" />
+      {Array(3)
+        .fill(null)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={`-z-10 h-0.5 w-full rounded-full bg-primary-600 transition-all duration-700 ${navIsOpen ? "opacity-0" : "opacity-1"}`}
+          />
+        ))}
 
-      {createPortal(
-        <aside
-          onClick={handleToggle}
-          className={`fixed top-0 z-20 h-screen w-screen bg-black/50 transition-all duration-500 lg:hidden ${navIsOpen ? "right-0 w-full opacity-100" : "-right-full w-0 opacity-0"}`}
-        >
-          {children}
-        </aside>,
-        document.body,
-      )}
+      <aside
+        onClick={handleToggle}
+        className={`fixed top-0 z-20 h-screen w-screen bg-black/50 transition-all duration-500 lg:hidden ${navIsOpen ? "right-0 w-full opacity-100" : "-right-full w-0 opacity-0"}`}
+      >
+        {children}
+      </aside>
     </div>
-  ) : null;
+  );
 }
